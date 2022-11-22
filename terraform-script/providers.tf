6f4291e4686665ec6942048a5d9518afabb53807
devanshu1github <ourextrawork2@gmail.com>
2022-11-22 05:06:58 -0800

    app code with Dockerfile and Jenkinsfile

---
+++ terraform-script/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+      aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.38"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.15"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
